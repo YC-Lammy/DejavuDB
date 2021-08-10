83366aff96cfd303fec1526150235fdc5aa34ae4
YC-L <game0427game@gmail.com>
2021-08-10 16:31:53 +0800

    javascript api

--- src/javascript.go
+++ src/javascript.go
@@ -1,35 +1,10 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"time"
 
 	"rogchap.com/v8go"
 )
-
-func javascript_context_init(ctx *v8go.Context, errs chan error) {
-
-	vm, _ := ctx.Isolate()
-
-	includefn, _ := v8go.NewFunctionTemplate(vm,
-		func(info *v8go.FunctionCallbackInfo) *v8go.Value { // when the JS function is called this Go callback will execute
-
-			switch v := fmt.Sprintf("%v", info.Args()); v {
-			case `"dejavu"`, `dejavu`, `'dejavu'`:
-				ctx.RunScript(javascript_API_Script, "dejavuDB.js")
-				val, _ := ctx.Global().Get("dejavu_api_class")
-				return val
-
-			default:
-				errs <- errors.New("could not import" + v + " (cannot find package " + v + " in any of path)")
-
-			}
-			return nil // you can return a value back to the JS caller if required
-		})
-
-	ctx.Global().Set("include", includefn) // register function
-}
 
 func javascript_run_isolate(script string, args ...interface{}) (string, error) {
 	vals := make(chan *v8go.Value, 1)
@@ -44,7 +19,7 @@ func javascript_run_isolate(script string, args ...interface{}) (string, error) 
 
 	go func() {
 
-		javascript_context_init(ctx, errs)
+		javascript_context_init(ctx, errs) // initiallize context api and functions
 
 		val, err := ctx.RunScript(script, "main.js") // exec a long running script
 		if err != nil {
--- src/javascriptAPI.go
+++ src/javascriptAPI.go
@@ -1,10 +1,79 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+
+	"rogchap.com/v8go"
+)
+
 const javascript_API_Script = `
+class dejavu_api_settings {
+	constructor(){}
+
+	static is_ML_enabled(){
+		return dejavu_api_is_ML_enabled()
+	}
+
+	static enable_ML(){
+		dejavu_api_enable_ML()
+	}
+
+	static disable_ML(){
+		
+	}
+}
+
 class dejavu_api_class {
 	constructor(){
 
 	}
 	static version = "dejavuDB 0.2.1"
+	static version_info = ""
+
+	static settings = dejavu_api_settings
 }
 `
+
+func javascript_context_init(ctx *v8go.Context, errs chan error) {
+
+	vm, _ := ctx.Isolate()
+	glob := ctx.Global()
+
+	includefn, _ := v8go.NewFunctionTemplate(vm,
+		func(info *v8go.FunctionCallbackInfo) *v8go.Value { // when the JS function is called this Go callback will execute
+
+			switch v := fmt.Sprintf("%v", info.Args()); v {
+			case `"dejavu"`, `dejavu`, `'dejavu'`, `"dejavu.js"`, `dejavu.js`, `'dejavu.js'`:
+				ctx.RunScript(javascript_API_Script, "dejavuDB.js")
+				val, _ := ctx.Global().Get("dejavu_api_class")
+				return val
+
+			default:
+				errs <- errors.New("could not import" + v + " (cannot find package " + v + " in any of path)")
+
+			}
+			return nil // you can return a value back to the JS caller if required
+		})
+
+	glob.Set("include", includefn) // register function
+
+	dejavu_api_is_ML_enabled, _ := v8go.NewFunctionTemplate(vm,
+
+		func(info *v8go.FunctionCallbackInfo) *v8go.Value {
+
+			v, _ := v8go.NewValue(vm, Settings.enable_ML)
+			return v
+		})
+
+	glob.Set("dejavu_api_is_ML_enabled", dejavu_api_is_ML_enabled)
+
+	dejavu_api_enable_ML, _ := v8go.NewFunctionTemplate(vm,
+
+		func(info *v8go.FunctionCallbackInfo) *v8go.Value {
+			Settings.enable_ML = true
+			return nil
+		})
+
+	glob.Set("dejavu_api_enable_ML", dejavu_api_enable_ML)
+}
