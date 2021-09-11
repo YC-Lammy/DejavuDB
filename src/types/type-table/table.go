1751a2abd11475e5303738d6e95891dac41b91cc
YC-Lammy <game0427game@gmail.com>
2021-09-11 19:47:42 +0100

    main

--- src/types/type-table/table.go
+++ src/types/type-table/table.go
@@ -1,1 +1,13 @@
 package table
+
+type Table struct {
+	Name       string
+	Columns    map[string]*Column
+	permission [3]uint8
+}
+
+type Column struct {
+	Name  string
+	Dtype byte
+	Data  []interface{}
+}
---
+++ src/types/types.go
@@ -0,0 +1,6 @@
+package types
+
+const (
+	String = 0x00
+	Int    = 0x01
+)
