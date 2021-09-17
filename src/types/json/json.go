45765cef21120e819668e8f49c6381317f43598b
YC-Lammy <game0427game@gmail.com>
2021-09-17 08:24:51 +0100

    main

--- src/types/json/json.go
+++ src/types/json/json.go
@@ -1,13 +1,12 @@
 package typejson
 
 import (
-	"rogchip.com/v8go"
+	"bytes"
+	"encoding/binary"
+	"strconv"
 
-	"../../lazy"
 	"../../types"
 )
-
-var vm *v8go.Context
 
 type BinaryJson [][]byte
 
@@ -33,49 +32,123 @@ func (b BinaryJson) String() string {
 	i := 0
 	for i < l {
 		r += "'" + string(b[i]) + "':"
-		r += string(b[i+1])
+		r += GetElemStr(b[i+1])
 		i += 2
 	}
-	return ""
+	r += "}"
+	return r
 }
 
-func (b BinaryJson) GetElemStrByIndex(i int) string {
+func GetElemStr(b []byte) string {
 	var a string
-	switch b[i][0] {
+	switch b[0] {
 	case types.Bool:
-		switch b[i][1] {
+		switch b[1] {
 		case 0x00:
 			a = "false"
 		case 0x01:
 			a = "true"
 		}
 	case types.Float64:
+		var length float64
+		buf := bytes.NewReader(b[1:])
+		binary.Read(buf, binary.LittleEndian, &length)
+		a = strconv.FormatFloat(length, 'f', 15, 64)
 	case types.String:
+		a = "'" + string(b[1:]) + "'"
 	case types.Array_interface: // array
+		// each elem separated by ";%@"
+
+		buf := [][]byte{}
+		sepc := 0
+		bufc := 0
+
+		for _, i := range b[1:] {
+			switch i {
+			case ';':
+				if sepc != 0 {
+					sepc = 0
+					buf[bufc] = append(buf[bufc], i)
+				} else {
+					sepc += 1
+				}
+
+			case '%':
+				if sepc != 1 {
+					sepc = 0
+					buf[bufc] = append(buf[bufc], i)
+				} else {
+					sepc += 1
+				}
+			case '@':
+				if sepc != 2 {
+					sepc = 0
+					buf[bufc] = append(buf[bufc], i)
+				} else {
+					bufc += 1
+					sepc = 0
+				}
+
+			default:
+				buf[bufc] = append(buf[bufc], i)
+			}
+		}
+		a += "["
+
+		for _, v := range buf {
+			a += GetElemStr(v) + ","
+		}
+		a += "]"
+
 	case types.Map_string_interface: // json
+		// each elem separated by ";%@"
+
+		buf := [][]byte{}
+		sepc := 0
+		bufc := 0
+
+		for _, i := range b[1:] {
+			switch i {
+			case ';':
+				if sepc != 0 {
+					sepc = 0
+					buf[bufc] = append(buf[bufc], i)
+				} else {
+					sepc += 1
+				}
+
+			case '%':
+				if sepc != 1 {
+					sepc = 0
+					buf[bufc] = append(buf[bufc], i)
+				} else {
+					sepc += 1
+				}
+			case '@':
+				if sepc != 2 {
+					sepc = 0
+					buf[bufc] = append(buf[bufc], i)
+				} else {
+					bufc += 1
+					sepc = 0
+				}
+
+			default:
+				buf[bufc] = append(buf[bufc], i)
+			}
+		}
+		a += "{"
+		l := len(buf)
+		i := 0
+		for i < l {
+			a += "'" + string(buf[i]) + "':"
+			a += GetElemStr(buf[i+1])
+			i += 2
+		}
+		a += "}"
+
 	case types.Null:
 		a = "null"
 	}
 	return a
-}
-
-type json struct {
-	location [8]byte
-}
-
-func NewJson(str string) (*json, error) {
-	s := lazy.RandString(8)
-	vm.RunScript(s+"="+str, "json.js")
-	var loc [8]byte
-	copy(loc[:], s)
-	return &json{location: loc}, nil
-}
-
-func (j json) Delete() {
-	vm.RunScript("delete "+string(j.location[:]), "delete.js")
-}
-
-func (j json) String() string {
-	val, _ := vm.RunScript(j.location, "get.js")
-	return val.String()
 }
